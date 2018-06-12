3b1db64bc9b6652e144844e4ba75862f0aa3d39a
PerlaMoly <40188451+PerlaMoly@users.noreply.github.com>
2018-06-11 22:51:25 -0300

    V1.0

---
+++ TP_Inserts.sql
@@ -0,0 +1,61 @@
+/* Insert tabla Persona*/
+/*Argentina*/
+
+insert into persona(cod_persona,nombre,tipo)
+values
+(001,'Willy Caballero','Participante'),
+(002,'Franco Armani','Participante'),
+(003,'Nahuel Guzmán','Participante'),
+(004,'Gabriel Mercado','Participante'),
+(005,'Eduardo Salvio','Participante'),
+(006,'Federico Fazio','Participante'),
+(007,'Nicolás Otamendi','Participante'),
+(008,'Marcos Acuña','Participante'),
+(009,'Nicolás Tagliafico','Participante'),
+(010,'Marcos Rojo','Participante'),
+(011,'Javier Mascherano','Participante'),
+(012,'Cristian Ansaldi','Participante'),
+(013,'Enzo Pérez','Participante'),
+(014,'Lucas Biglia','Participante'),
+(015,'Giovani Lo Celso','Participante'),
+(016,'Ángel Di María','Participante'),
+(017,'Éver Banega','Participante'),
+(018,'Maximiliano Meza','Participante'),
+(019,'Cristian Pavón','Participante'),
+(020,'Leo Messi','Participante'),
+(021,'Paulo Dybala','Participante'),
+(022,'Sergio Agüero','Participante'),
+(023,'Gonzalo Higuaín','Participante')
+
+/* */
+insert into persona(cod_persona,nombre,tipo)
+values
+(default,'Subasic','Participante'),
+(default,'Kalinic','Participante'),
+(default,'Livakovic','Participante'),
+(default,'Vedran Corluka','Participante'),
+(default,'Domagoj Vida','Participante'),
+(default,'Ivan Strinic','Participante'),
+(default,'Dejan Lovren','Participante'),
+(default,'Sime Vrsaljko','Participante'),
+(default,'Josip Pivaric','Participante'),
+(default,'Tin Jedvaj','Participante'),
+(default,'Matej Mitrovic','Participante'),
+(default,'Caleta-Car','Participante'),
+(default,'Luka Modric','Participante'),
+(default,'Mateo Kovacic','Participante'),
+(default,'Ivan Rakitic','Participante'),
+(default,'Milan Badelj','Participante'),
+(default,'Marcelo Brozovic','Participante'),
+(default,'Filip Bradaric','Participante'),
+(default,'Mario Mandzukic','Participante'),
+(default,'Ivan Perisic','Participante'),
+(default,'Nikola Kalinic','Participante'),
+(default,'Andrej Kramaric','Participante'),
+(default,'Marko Pjaca','Participante'),
+(default,'Ante Rebic','Participante');
+
+
+
+select * from persona;
+
